99894305284f90dd883e3b398fc184f03ef6aeba
Igor Cordeiro <ifcigor@gmail.com>
2024-06-23 12:57:10 -0300

    Second test created, should read the conteudo created before

--- test/conteudosTests/test-conteudo.test.js
+++ test/conteudosTests/test-conteudo.test.js
@@ -21,23 +21,40 @@ describe('Testes integrados da toda de conteúdos', () => {
         const response = await request(rotaUsers)
             .post('/conteudos')
             .send(payload_cadastro_conteudo);
-            console.log(response.body);
-            const{titulo, descricao, tipoConteudo, conteudo} = response.body;
-            console.log(`titulo: ${titulo}, descricao: ${descricao}, tipoConteudo: ${tipoConteudo}, conteudo: ${conteudo}`);
+        console.log(response.body);
 
-            // salva o id para utilizar em testes de alteração e deleção
-            idConteudoCadastrado = response.body.id;
+        //desestruturação do response para poder validar os dados recebidos com enviados
+        const { titulo, descricao, tipoConteudo, conteudo, carro } = response.body;
 
-            
+        // salva o id para utilizar em testes de alteração e deleção
+        idConteudoCadastrado = response.body.id;
 
-            // verifica se o conteudo está retornando os dados esperados
-            expect(titulo).toBe(payload_cadastro_conteudo.titulo);
-            expect(descricao).toBe(payload_cadastro_conteudo.descricao);
-            expect(tipoConteudo).toBe(payload_cadastro_conteudo.tipoConteudo);
-            expect(conteudo).toBe(payload_cadastro_conteudo.conteudo);
+        // verifica se o conteudo está retornando os dados esperados
+        expect(titulo).toBe(payload_cadastro_conteudo.titulo);
+        expect(descricao).toBe(payload_cadastro_conteudo.descricao);
+        expect(tipoConteudo).toBe(payload_cadastro_conteudo.tipoConteudo);
+        expect(conteudo).toBe(payload_cadastro_conteudo.conteudo);
 
+        // verifica se o status code está correto
+        expect(response.status).toBe(201);
+    });
 
-            // verifica se o status code está correto
-            expect(response.status).toBe(201);
+    it('Consultar item cadastrado e verificar se os dados estão corretos', async () => {
+        const response = await request(rotaUsers)
+            .get(`/conteudos/${idConteudoCadastrado}`);
+        console.log(response.body);
+    
+        //desestruturação do response para poder validar os dados recebidos com enviados
+        const { id, titulo, descricao, tipoConteudo, conteudo, carro } = response.body;
+
+        // valida se o status code está correto dessa consulta
+        expect(response.status).toBe(200);
+        
+        // verifica se os dados buscados são os mesmo que os enviados no cadastro
+        expect(id).toBe(idConteudoCadastrado);
+        expect(titulo).toBe(payload_cadastro_conteudo.titulo);
+        expect(descricao).toBe(payload_cadastro_conteudo.descricao);
+        expect(tipoConteudo).toBe(payload_cadastro_conteudo.tipoConteudo);
+        expect(conteudo).toBe(payload_cadastro_conteudo.conteudo);
     });
 });
